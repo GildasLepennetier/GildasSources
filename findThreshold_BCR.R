6789afb442c4ba7d360c712921529f668bdfdadc
Gildas Lepennetier <gildas.lepennetier@hotmail.fr>
2016-11-29 13:13:02 +0100

    in devel

---
+++ findThreshold_BCR.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+
+# function to use as replacement of shazam::findThreshold
+# very big approximation using median + two standard deviation.
+
+##################################################
+# Gildas Lepennetier 2016.11.22
+library(shazam,q=T)
+library(alakazam,q=T)
+MODEL=c(" hs1f", "hs5f", "ham", "aa", "m1n", "m3n")[3]
+ARGS=commandArgs(TRUE)
+if(length(ARGS) < 1){
+	write("threshold approximation by: median + 2*sd", stderr())
+	write(paste("Using model of distance:",MODEL), stderr())	
+	write(paste("usage: Rscript --vanilla <script threshold_perso> <files ...>"), stderr())
+}else{
+	for (file in ARGS){
+		if(file.exists(file)){
+			db <- readChangeoDb(file)
+			db <- distToNearest(db, model=MODEL, first=FALSE)
+			threshold_perso = median(db$DIST_NEAREST,na.rm=T) + 2*sd(db$DIST_NEAREST,na.rm=T)
+			write(paste(signif(threshold_perso,4),basename(file)), stdout())
+		}else{
+			write(paste("NA",file), stderr())
+		}
+	}
+	quit("no")
+}
