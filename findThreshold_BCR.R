ba20e11c2f135debbfdbc206a7ec66872af7e8fb
Gildas Lepennetier <gildas.lepennetier@hotmail.fr>
2016-12-09 12:27:58 +0100

    changed 2*sd -> 1.5*sd

--- findThreshold_BCR.R
+++ findThreshold_BCR.R
@@ -10,7 +10,7 @@ library(alakazam,q=T)
 MODEL=c(" hs1f", "hs5f", "ham", "aa", "m1n", "m3n")[3]
 ARGS=commandArgs(TRUE)
 if(length(ARGS) < 1){
-	write("threshold approximation by: median + 2*sd", stderr())
+	write("threshold approximation by: median + 1.5*sd", stderr())
 	write(paste("Using model of distance:",MODEL), stderr())	
 	write(paste("usage: Rscript --vanilla <script threshold_perso> <files ...>"), stderr())
 }else{
@@ -18,7 +18,7 @@ if(length(ARGS) < 1){
 		if(file.exists(file)){
 			db <- readChangeoDb(file)
 			db <- distToNearest(db, model=MODEL, first=FALSE)
-			threshold_perso = median(db$DIST_NEAREST,na.rm=T) + 2*sd(db$DIST_NEAREST,na.rm=T)
+			threshold_perso = median(db$DIST_NEAREST,na.rm=T) + 1.5*sd(db$DIST_NEAREST,na.rm=T)
 			write(paste(signif(threshold_perso,4),basename(file)), stdout())
 		}else{
 			write(paste("NA",file), stderr())
