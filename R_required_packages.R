c0d021be10653b09fa10e82c102715f0ab6a349a
Gildas Lepennetier <gildas.lepennetier@hotmail.fr>
2017-06-20 16:08:19 +0200

    updated r function

--- R_required_packages.R
+++ R_required_packages.R
@@ -34,76 +34,57 @@ install.packages(c("devtools", "roxygen2", "testthat", "knitr", "rmarkdown", "Rc
 
 # functions
 count=function(v){return(length(na.omit(v)))}
-breadth=function(v,lim=F){if(lim){v=v[v<lim]};return(length(na.omit(v))/length(v))}
-TSI=function(v,w=F){v=na.omit(v);if(length(v)<2){if(w){write("Error in TSI: you need a vector of size > 1",stderr())};return(NA)};if(!sum(v!=0)){write("Error in TSI: 1 value should be different than 0",stderr());return(NA)};return(sum(1-(v/max(v)))/(length(v)-1))} #tissue specificity index from Yanai 2005, min=0, max=1, > 1tissue, 1 value !=0
+#from p-values, return stars
 stars=function(p.value,limits=c(0.05,0.01,0.001),sign=2,NS=T){if(NS){star="NS"}else{star=signif(p.value,sign)}; if(p.value<limits[1]){star="*"};if(p.value<limits[2]){star="**"};if(p.value<limits[3]){star="***"};return(star)}
+#samples
 sampleRows = function(df,n,r=T){return(df[sample(nrow(df),n,replace=r),])} #sample n rows in a df, with/wo replacement
-#you can give to asymmetry two verctor of count, but not two vector of position, carefull !
-asymmetry=function(sens,anti){if(length(sens)!=length(anti)){print("Error: sens and anti should have the same length",stderr());return(NA)};s=sum(sens);a=sum(anti);if(s==0&a==0){return (0)}else{return((s-a)/(s+a))}}#take into account 0 obs in both strands, and work with vectors or integers
-
-asymmetryBatch=function(sens,anti){
-	if(length(sens)!=length(anti)){
-		print("Error: sens and anti should have the same length",stderr())
-		return(NA)
+#from a vector, returen a vector of rainbow colors
+vector_to_color=function(x, colors=rainbow(length(unique(x))), default="black"){
+	if( length(colors) < length(unique(x))){
+		print(paste("Error: only",length(colors),"colors provided, but required",length(unique(x))))
+		print("Appying default color selection using rainbow")
+		colors=rainbow(length(unique(x)))
 	}
-	RETURN=c()
-	for( i in 1:length(sens)){
-		s=sens[i]
-		a=anti[i]
-		RETURN=c( RETURN, (s-a)/(s+a) )
+	y=rep(default, length(x))
+	for( index in 1:length(unique(x))){y [ x == unique(x)[index] ] = colors[index]}
+	return(y)
+}
+factor_to_colors=function(fact, colors=rainbow(length(unique(x))), default="black"){
+	LVL=levels(fact) ; NB=length(LVL)
+	if( length(colors) < NB ){
+		print(paste("Error: only",length(colors),"colors provided, but required",NB))
+		print("Appying default color selection using rainbow")
+		colors=rainbow(NB)
 	}
-	return(RETURN)
+	COL=rep(default,length(fact))
+	for(index in 1:NB){COL [ fact == LVL[index] ] = colors[ index ]}
+	return(COL)
+}
+vector_to_pch=function(x, pchs, default=16){
+	# pch: 0:14 = no fill
+	# pch 15:20 = filled
+	# pch 21:25 = bg + color of line
+	# pch=26:32 : DOES NOT EXIST
+	# pch=32:255 give the text symbol in a single-byte locale
+	POSSIBLE=c(0:25,32:255)
+	if( length(pchs) > length(POSSIBLE)){print("Error: not possible to use more than 250 different pch"); return(NULL)}
+	if( length(pchs) < length(unique(x))){
+		print(paste("Error: only",length(pchs),"pchs provided, but required",length(unique(x))))
+		print("Appying default selection")
+		pchs=POSSIBLE[ 1:length(unique(x)) ] #selection on possible values
+	}
+	y=rep(default, length(x))
+	for( index in 1:length(unique(x))){y [ x == unique(x)[index] ] = pchs[index]}
+	return(y)
 }
 
+#convertion string to number of minutes
 time_string_to_minutes=function(string,Split=":"){
 	"conversion of 1:50, or 1h50 in minutes (110 here)"
 	LI=strsplit(string, split = Split)
 	return( unlist(lapply(LI, function(v){return( as.integer(v[1])*60 + as.integer(v[2]))} ) ) )
 }
-
-motif_from_end=function(L,V){ #L=list with positionS of PA in exon - V=vector of exon size as reference
-	if(length(L)!=length(V)){write("Error, list and vector should be of same length",stderr())}
-	OUT=c()
-	for (kk in 1:length(V)){
-		l=as.integer(L[[ kk ]]) # get list of position
-		v=as.integer(V[  kk  ]) # get corresponding gene size
-		if(length(l)==0){OUT=c(OUT,NA)}
-		if(length(l)==1){OUT=c(OUT, v-l )}
-		if(length(l)>1){l=l[length(l)];OUT=c(OUT, v-l )}#only the last poly-A in exon
-	}
-	return(OUT)
-}
-motif_first=function(L){ #L=list with positionS of PA in exon - V=vector of exon size as reference
-	OUT=c()
-	for (kk in 1:length(L)){
-		l=as.integer(L[[ kk ]]) # get list of position
-		if(length(l)==0){OUT=c(OUT,NA)}
-		if(length(l)==1){OUT=c(OUT, l )}
-		if(length(l)>1){l=l[1];OUT=c(OUT, l )}#only the last poly-A in exon
-	}
-	return(OUT)
-}
-motif_last=function(L){ #L=list with positionS of PA in exon - V=vector of exon size as reference
-	OUT=c()
-	for (kk in 1:length(L)){
-		l=as.integer(L[[ kk ]]) # get list of position
-		if(length(l)==0){OUT=c(OUT,NA)}
-		if(length(l)==1){OUT=c(OUT, l )}
-		if(length(l)>1){l=l[length(l)];OUT=c(OUT,l)}#only the last poly-A in exon
-	}
-	return(OUT)
-}
-motif_expand=function(L,split=",",toint=F){
-	OUT=c()
-	for (kk in 1:length(L)){
-		OUT=c(OUT, unlist(  strsplit(L[kk],split = split) ) )
-	}
-	if(toint){
-		return( as.integer(OUT) )
-	}else{
-		return( OUT )
-	}
-}
+#insert spaces between two list of words
 alignWords = function(l1,l2,sep=" ",minspace=2){
 	if(minspace<0){simpleError("error, minimum space is 0")}
 	M1 =nchar(l1)
@@ -113,147 +94,19 @@ alignWords = function(l1,l2,sep=" ",minspace=2){
 	l3 = sapply(X = DIFF, FUN = function(x,sep){return(paste0(rep(sep,x),collapse=""))}, sep)
 	return( paste0( l1, l3, l2 ) )
 }
-
-# calculate the position in percentage from start using position of motif, size of element. If several motif, use all="all"
-motif_percent_from_start=function(L,V,all="all"){
-	#L=list with positionS of PA in exon - V=vector of size as reference
-	if(length(L)!=length(V)){write("Error, list and vector should be of same length",stderr());return(NA)}
-	OUT=c()
-	for (kk in 1:length(V)){
-		l=as.integer(L[[ kk ]]) # get list of position
-		v=as.integer(V[  kk  ]) # get corresponding gene size
-		if(length(l)==0){OUT=c(OUT,NA)} # keep NA if no motif
-		if(length(l)==1){OUT=c(OUT, l/v )} # if one motif, it's fine
-		# if more than one motif:
-		if(length(l)>1){
-			if(all=="all"){
-				for( xx in l){
-					OUT=c(OUT,  xx /v ) # need to calculate the percentage relative to each reference size
-				}
-			}
-			if(all=="first"){
-				l=l[1] #select first
-				OUT=c(OUT,  l/v )
-			}
-			if(all=="last"){
-				l=l[length(l)] #select last
-				OUT=c(OUT,  l/v )
-			}#only the last poly-A in exon
-		}
-	}
-	#
-	return(OUT)
-}
-#extract attribute from a GFF file
-ExtactMatch = function(string,field="gene_id",sep=" ",pattern="[[:alnum:]]*"){
-	Regexp <- paste0(field,sep,pattern) #create the regexp to match
-	MATCH=regmatches(string,regexpr(Regexp,string)) #extract the match using the regexp
-	return(strsplit(x=MATCH,split=sep,fixed=T)[[1]][2]) #print the match described in pattern attribute
-	"USAGE: Give a string, this function will extract the pattern associated with the field after a sep.
-	author: Gildas Lepennetier 2015
-	"
-}
-#extract all attribute from a GFF file
-getAttributeField = function(attributes,field="gene_id",sep=" ",pattern="[[:alnum:]]*"){
-	return( sapply(X=attributes,FUN=ExtactMatch,field,sep,pattern,USE.NAMES=F))#do not use name, we want only the match
-	"USAGE: Give a vector of GFF attribute.
-	This function will extract the pattern associated with the field after a sep.
-	author: Gildas Lepennetier 2015
-	"
-}
 # reverse a string
 reverse=function(string){return(sapply(lapply(strsplit(string, NULL), rev), paste, collapse=""))}
 # complement
-complement_OLD=function(string, type="DNA"){
-	DNA=function(letter){
-		if(letter=="A") comp<-"T"
-		if(letter=="C") comp<-"G"
-		if(letter=="G") comp<-"C"
-		if(letter=="T") comp<-"A"
-		if(letter=="a") comp<-"t"
-		if(letter=="c") comp<-"g"
-		if(letter=="g") comp<-"c"
-		if(letter=="t") comp<-"a"
-		if(letter=="Y") comp<-"R"
-		if(letter=="y") comp<-"r"
-		if(letter=="R") comp<-"Y"
-		if(letter=="r") comp<-"y"
-		if(letter=="S") comp<-"S"
-		if(letter=="s") comp<-"s"
-		if(letter=="W") comp<-"W"
-		if(letter=="w") comp<-"w"
-		if(letter=="K") comp<-"M"
-		if(letter=="k") comp<-"m"
-		if(letter=="M") comp<-"K"
-		if(letter=="m") comp<-"k"
-		if(letter=="B") comp<-"V"
-		if(letter=="b") comp<-"v"
-		if(letter=="D") comp<-"H"
-		if(letter=="d") comp<-"h"
-		if(letter=="H") comp<-"D"
-		if(letter=="h") comp<-"d"
-		if(letter=="V") comp<-"B"
-		if(letter=="v") comp<-"b"
-		if(letter=="N") comp<-"N"
-		if(letter=="n") comp<-"n"
-		return(comp)
-	}
-	RNA=function(letter){
-		if(letter=="A") comp<-"U"
-		if(letter=="C") comp<-"G"
-		if(letter=="G") comp<-"C"
-		if(letter=="U") comp<-"A"
-		if(letter=="a") comp<-"u"
-		if(letter=="c") comp<-"g"
-		if(letter=="g") comp<-"c"
-		if(letter=="u") comp<-"a"
-		if(letter=="Y") comp<-"R"
-		if(letter=="y") comp<-"r"
-		if(letter=="R") comp<-"Y"
-		if(letter=="r") comp<-"y"
-		if(letter=="S") comp<-"S"
-		if(letter=="s") comp<-"s"
-		if(letter=="W") comp<-"W"
-		if(letter=="w") comp<-"w"
-		if(letter=="K") comp<-"M"
-		if(letter=="k") comp<-"m"
-		if(letter=="M") comp<-"K"
-		if(letter=="m") comp<-"k"
-		if(letter=="B") comp<-"V"
-		if(letter=="b") comp<-"v"
-		if(letter=="D") comp<-"H"
-		if(letter=="d") comp<-"h"
-		if(letter=="H") comp<-"D"
-		if(letter=="h") comp<-"d"
-		if(letter=="V") comp<-"B"
-		if(letter=="v") comp<-"b"
-		if(letter=="N") comp<-"N"
-		if(letter=="n") comp<-"n"
-		return(comp)
-	}
-	if(type=="DNA") {
-		tryCatch(expr = return( paste(unlist(lapply( unlist(strsplit(string,NULL)) , DNA) ),collapse="")), error = function(w){print("Error: this is not DNA")})
-		}
-	if(type=="RNA") {
-		tryCatch(expr = return( paste(unlist(lapply( unlist(strsplit(string,NULL)) , RNA) ),collapse="")), error = function(w){print("Error: this is not RNA")})
-		}
-}
-
 complement=function(string){return(chartr(old = "AaTtCcGgRrYySsWwKkMmBbVvDdHh",new = "TtAaGgCcYyRrWwSsMmKkVvBbHhDd",x = string))}
 # reverse complement
 reverseComplement=function(string, type="DNA"){return(complement(reverse(string),type=type))}
 # test 	reverseComplement( "ATG" ) == "CAT"
 
-values_between = function(string,start,end,sep=','){
-    line = as.integer( unlist( strsplit(string,fixed=T,split=sep) ) )
-    return( line[ line >= start & line <= end ] )
-    #from a string coma separated integer ('1,2,50,100')-> return vector of integer in range start-end (ex: start = 2, will remove value 1)
-}
+round_below=function(x, r=1){ return( floor(x) - floor(x) %% r)	}
+round_above=function(x, r=1){ return( ceiling(x) + ceiling(x) %% r) }
 
-values_between_count=function( V, start, end ){
-    return(count(unlist( foreach(line = V ) %do%  values_between(line, start, end) )))
-    #given a column (can contain NA), return the count of motifs between start and end
-}
+ExpandPreviousOnNA=function(x){"This function replace the NAs of a vector by the previous non-NA value";ELEM=NA;VECT=c();for ( index in 1:length(x)){if( ! is.na(x[index]) ){ELEM=x[index]} ; VECT=c(VECT,ELEM)} ; return(VECT)}
+
 
 #map element of a vectors without duplicates
 #old version, keep just in case
@@ -262,6 +115,13 @@ values_between_count=function( V, start, end ){
 pair_map=function(vect){V1=c();V2=c();for (index1 in 1:(length(vect)-1) ){for ( index2 in ((index1):length(vect))){V1=c(V1,vect[ index1]);V2=c(V2,vect[ index2]);};};return(list(index1=V1,index2=V2,names=vect))}
 #return map of comparisons for two vectors, without duplicated comparison
 pair_map2=function(vect1, vect2){V1=c();V2=c();for (index1 in 1:(length(vect1)-1) ){for ( index2 in ((index1):length(vect2))){V1=c(V1,vect1[ index1]);V2=c(V2,vect2[ index2]);};};return(list(index1=V1,index2=V2,names1=vect1,names2=vect2))}
+
+#function to sum in range (1 by 1) from 1 to N ##Leonhard Euler
+sum_serie_1_to_N=function(N){return( (N * (N + 1)) /2 )}
+#function to sum in range (1 by 1) from N1 to N2, including or expluding those limits
+sum_serie_range_inclusively=function(from,to){return( sum_serie_1_to_N(to) - sum_serie_1_to_N(from-1) )}
+sum_serie_range_exclusively=function(from,to){return( sum_serie_1_to_N(to-1) - sum_serie_1_to_N(from) )}
+
 
 # hamming distance
 hamming=function(str1, str2){return( sum(strsplit(str1,split = "",fixed = T)[[1]] != strsplit(str2,split = "",fixed = T)[[1]]))}
@@ -281,28 +141,131 @@ normalize_know_range <- function(x,xmin,xmax) {
 	return ( (x-xmin) / (xmax-xmin) )
 }
 
-#function to sum in range (1 by 1) from 1 to N ##Leonhard Euler
-sum_serie_1_to_N=function(N){
-	return( (N * (N + 1)) /2 )
-}
-#function to sum in range (1 by 1) from N1 to N2, including or expluding those limits
-sum_serie_range_inclusively=function(from,to){
-	return( sum_serie_1_to_N(to) - sum_serie_1_to_N(from-1) )
-}
-sum_serie_range_exclusively=function(from,to){
-	return( sum_serie_1_to_N(to-1) - sum_serie_1_to_N(from) )
+
+
+# specific functions used during my thesis
+if ( F ){ 
+	breadth=function(v,lim=F){if(lim){v=v[v<lim]};return(length(na.omit(v))/length(v))}
+	TSI=function(v,w=F){v=na.omit(v);if(length(v)<2){if(w){write("Error in TSI: you need a vector of size > 1",stderr())};return(NA)};if(!sum(v!=0)){write("Error in TSI: 1 value should be different than 0",stderr());return(NA)};return(sum(1-(v/max(v)))/(length(v)-1))} #tissue specificity index from Yanai 2005, min=0, max=1, > 1tissue, 1 value !=0
+	#you can give to asymmetry two verctor of count, but not two vector of position, carefull !
+	asymmetry=function(sens,anti){if(length(sens)!=length(anti)){print("Error: sens and anti should have the same length",stderr());return(NA)};s=sum(sens);a=sum(anti);if(s==0&a==0){return (0)}else{return((s-a)/(s+a))}}#take into account 0 obs in both strands, and work with vectors or integers
+	asymmetryBatch=function(sens,anti){
+		if(length(sens)!=length(anti)){
+			print("Error: sens and anti should have the same length",stderr())
+			return(NA)
+		}
+		RETURN=c()
+		for( i in 1:length(sens)){
+			s=sens[i]
+			a=anti[i]
+			RETURN=c( RETURN, (s-a)/(s+a) )
+		}
+		return(RETURN)
+	}
+	motif_from_end=function(L,V){ #L=list with positionS of PA in exon - V=vector of exon size as reference
+		if(length(L)!=length(V)){write("Error, list and vector should be of same length",stderr())}
+		OUT=c()
+		for (kk in 1:length(V)){
+			l=as.integer(L[[ kk ]]) # get list of position
+			v=as.integer(V[  kk  ]) # get corresponding gene size
+			if(length(l)==0){OUT=c(OUT,NA)}
+			if(length(l)==1){OUT=c(OUT, v-l )}
+			if(length(l)>1){l=l[length(l)];OUT=c(OUT, v-l )}#only the last poly-A in exon
+		}
+		return(OUT)
+	}
+	motif_first=function(L){ #L=list with positionS of PA in exon - V=vector of exon size as reference
+		OUT=c()
+		for (kk in 1:length(L)){
+			l=as.integer(L[[ kk ]]) # get list of position
+			if(length(l)==0){OUT=c(OUT,NA)}
+			if(length(l)==1){OUT=c(OUT, l )}
+			if(length(l)>1){l=l[1];OUT=c(OUT, l )}#only the last poly-A in exon
+		}
+		return(OUT)
+	}
+	motif_last=function(L){ #L=list with positionS of PA in exon - V=vector of exon size as reference
+		OUT=c()
+		for (kk in 1:length(L)){
+			l=as.integer(L[[ kk ]]) # get list of position
+			if(length(l)==0){OUT=c(OUT,NA)}
+			if(length(l)==1){OUT=c(OUT, l )}
+			if(length(l)>1){l=l[length(l)];OUT=c(OUT,l)}#only the last poly-A in exon
+		}
+		return(OUT)
+	}
+	motif_expand=function(L,split=",",toint=F){
+		OUT=c()
+		for (kk in 1:length(L)){
+			OUT=c(OUT, unlist(  strsplit(L[kk],split = split) ) )
+		}
+		if(toint){
+			return( as.integer(OUT) )
+		}else{
+			return( OUT )
+		}
+	}
+	# calculate the position in percentage from start using position of motif, size of element. If several motif, use all="all"
+	motif_percent_from_start=function(L,V,all="all"){
+		#L=list with positionS of PA in exon - V=vector of size as reference
+		if(length(L)!=length(V)){write("Error, list and vector should be of same length",stderr());return(NA)}
+		OUT=c()
+		for (kk in 1:length(V)){
+			l=as.integer(L[[ kk ]]) # get list of position
+			v=as.integer(V[  kk  ]) # get corresponding gene size
+			if(length(l)==0){OUT=c(OUT,NA)} # keep NA if no motif
+			if(length(l)==1){OUT=c(OUT, l/v )} # if one motif, it's fine
+			# if more than one motif:
+			if(length(l)>1){
+				if(all=="all"){
+					for( xx in l){
+						OUT=c(OUT,  xx /v ) # need to calculate the percentage relative to each reference size
+					}
+				}
+				if(all=="first"){
+					l=l[1] #select first
+					OUT=c(OUT,  l/v )
+				}
+				if(all=="last"){
+					l=l[length(l)] #select last
+					OUT=c(OUT,  l/v )
+				}#only the last poly-A in exon
+			}
+		}
+		#
+		return(OUT)
+	}
+	#extract attribute from a GFF file
+	ExtactMatch = function(string,field="gene_id",sep=" ",pattern="[[:alnum:]]*"){
+		Regexp <- paste0(field,sep,pattern) #create the regexp to match
+		MATCH=regmatches(string,regexpr(Regexp,string)) #extract the match using the regexp
+		return(strsplit(x=MATCH,split=sep,fixed=T)[[1]][2]) #print the match described in pattern attribute
+		"USAGE: Give a string, this function will extract the pattern associated with the field after a sep.
+		author: Gildas Lepennetier 2015
+		"
+	}
+	#extract all attribute from a GFF file
+	getAttributeField = function(attributes,field="gene_id",sep=" ",pattern="[[:alnum:]]*"){
+		return( sapply(X=attributes,FUN=ExtactMatch,field,sep,pattern,USE.NAMES=F))#do not use name, we want only the match
+		"USAGE: Give a vector of GFF attribute.
+		This function will extract the pattern associated with the field after a sep.
+		author: Gildas Lepennetier 2015
+		"
+	}
+	
+	values_between = function(string,start,end,sep=','){
+		line = as.integer( unlist( strsplit(string,fixed=T,split=sep) ) )
+		return( line[ line >= start & line <= end ] )
+		#from a string coma separated integer ('1,2,50,100')-> return vector of integer in range start-end (ex: start = 2, will remove value 1)
+	}
+	values_between_count=function( V, start, end ){
+		return(count(unlist( foreach(line = V ) %do%  values_between(line, start, end) )))
+		#given a column (can contain NA), return the count of motifs between start and end
+	}
+	
+	
 }
 
-round_below=function(x, r=1){ return( floor(x) - floor(x) %% r)	}
-round_above=function(x, r=1){ return( ceiling(x) + ceiling(x) %% r) }
-factor_to_colors=function(fact, default="black"){
-	LVL=levels(fact)
-	NB=length(LVL)
-	COL_LI=rainbow(NB)
-	COL=rep(default,length(fact))
-	for(index in seq(1,NB)){
-		COL [ fact == LVL[index] ] <- COL_LI[ index ]
-	}
-	return(COL)
-}
+
+
 
